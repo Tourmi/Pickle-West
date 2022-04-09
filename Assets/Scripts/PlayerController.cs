29d4913072e84eb4135f2bce76affc1a9e456e22
Patrick Bergeron <pbergeron@lunix.pro>
2022-04-09 04:10:18 -0400

    Update animation tree using blend tree Minor change to player collider height and sprite render order. Enable prefab autosave

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,28 +5,45 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+  const string ANIMATOR_LOOK = "direction";
+  const string ANIMATOR_MOVE = "move";
+  const string ANIMATOR_DAMAGE = "tookDamage";
+
   private Rigidbody2D rigidBody;
+  private Animator spriteAnimator;
+  private Vector2 move;
+  private float lookDirection = 0;
 
   [SerializeField]
   public float moveSpeed;
-  private Vector2 move;
 
   // Start is called before the first frame update
   void Start()
   {
     this.rigidBody = GetComponent<Rigidbody2D>();
+    this.spriteAnimator = GetComponentInChildren<Animator>();
   }
 
   // Update is called once per frame
   void Update()
   {
-    Debug.Log(this.move);
+    this.spriteAnimator.SetBool(
+      PlayerController.ANIMATOR_MOVE, (this.move.x != 0 || this.move.y != 0)
+    );
+    this.spriteAnimator.SetFloat(
+      PlayerController.ANIMATOR_LOOK, this.lookDirection
+    );
+
     this.rigidBody.MovePosition(this.rigidBody.position + move * this.moveSpeed * Time.deltaTime);
   }
 
   public void Move(InputAction.CallbackContext ctx)
   {
-
     this.move = ctx.ReadValue<Vector2>();
+    this.lookDirection =
+      (this.move.x == 0 && this.move.y == 0) ? this.lookDirection :
+      (this.move.y < 0) ? 0 :
+      (this.move.x > 0) ? 1 :
+      (this.move.y > 0) ? 2 : 3;
   }
 }
