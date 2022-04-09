c656d5d6b26443f51f7c07182ccd72e499b39a21
Tourmi <domino_b10@hotmail.com>
2022-04-09 17:54:04 -0400

    Bullet spread

--- Assets/Scripts/Attacks/GunBehaviour.cs
+++ Assets/Scripts/Attacks/GunBehaviour.cs
@@ -12,14 +12,17 @@ public class GunBehaviour : MonoBehaviour
   [SerializeField]
   private AttackBehaviour bullet;
   [SerializeField]
-  private Transform gunCannon;
+  private float cannonDistance;
+  [SerializeField]
+  private float spread;
+  [SerializeField]
+  private int bulletCount;
 
   // Start is called before the first frame update
   void Start()
   {
     if (bullet == null) Debug.LogError("Gun does not have a bullet assigned");
     if (owner == null) Debug.LogError("The gun owner is not assigned");
-    if (gunCannon == null) Debug.LogError("The gun canon is not assigned");
     baseAngle = this.transform.rotation.z;
   }
 
@@ -31,9 +34,34 @@ public class GunBehaviour : MonoBehaviour
 
   public void Shoot()
   {
+    if (bulletCount <= 1)
+    {
+      shootBullet(currDirection);
+      return;
+    }
+    float tempBulletCount = bulletCount;
+    if (spread >= 360) tempBulletCount++;
+
+    float angleBetweenBullets = spread / (tempBulletCount - 1);
+    float currentAngle = -spread / 2;
+    for (int i = 0; i < bulletCount; i++)
+    {
+      var sin = Mathf.Sin(currentAngle * Mathf.Deg2Rad);
+      var cos = Mathf.Cos(currentAngle * Mathf.Deg2Rad);
+
+      Vector2 dirr = new Vector2(cos * currDirection.x - sin * currDirection.y, sin * currDirection.x + cos * currDirection.y);
+      shootBullet(dirr);
+      currentAngle += angleBetweenBullets;
+    }
+
+  }
+
+  private void shootBullet(Vector2 direction)
+  {
     var bulletInstance = Instantiate(bullet);
-    bulletInstance.direction = this.currDirection;
-    bulletInstance.transform.position = gunCannon.position;
+    bulletInstance.direction = direction;
+    var cannonOffset = direction * cannonDistance;
+    bulletInstance.transform.position = this.transform.position + new Vector3(cannonOffset.x, cannonOffset.y);
     bulletInstance.owner = this.owner;
   }
 }
