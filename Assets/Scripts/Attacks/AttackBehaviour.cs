926c827aeb7ff590fee111aa088d5c36d0c49775
Patrick Bergeron <pbergeron@lunix.pro>
2022-04-10 04:47:52 -0400

    Fix crab bullet hitbox Fix exception in attackbehaviour.

--- Assets/Scripts/Attacks/AttackBehaviour.cs
+++ Assets/Scripts/Attacks/AttackBehaviour.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
 public class AttackBehaviour : MonoBehaviour
 {
+  private string ownerAlliance;
+
   [SerializeField]
   public GameObject owner;
 
@@ -24,14 +26,14 @@ public class AttackBehaviour : MonoBehaviour
   public Vector2 direction;
   private Rigidbody2D body;
 
-  private List<GameObject> alreadyHit;
+  private List<GameObject> alreadyHit = new List<GameObject>();
 
   // Start is called before the first frame update
   void Start()
   {
     this.body = GetComponent<Rigidbody2D>();
     direction = direction.normalized;
-    alreadyHit = new List<GameObject>();
+    this.ownerAlliance = this.owner.GetComponent<AllianceBehaviour>().alliance;
   }
 
   // Update is called once per frame
@@ -49,16 +51,13 @@ public class AttackBehaviour : MonoBehaviour
 
   private void OnTriggerEnter2D(Collider2D collider)
   {
-    if (this.owner == null) return;
-
     var other = collider.gameObject;
     var healthComponent = other.GetComponent<HealthBehaviour>();
-    AllianceBehaviour ownerAlliance = this.owner.GetComponent<AllianceBehaviour>();
     AllianceBehaviour otherAlliance = other.GetComponent<AllianceBehaviour>();
 
     bool otherHasHealth = healthComponent != null;
-    bool allHasAlliance = ownerAlliance != null && otherAlliance != null;
-    bool sameAlliance = allHasAlliance && ownerAlliance.alliance == otherAlliance.alliance;
+    bool allHasAlliance = otherAlliance != null;
+    bool sameAlliance = allHasAlliance && this.ownerAlliance == otherAlliance.alliance;
     bool wasAlreadyHit = alreadyHit.Contains(other);
 
     if (otherHasHealth && !sameAlliance && !wasAlreadyHit)
